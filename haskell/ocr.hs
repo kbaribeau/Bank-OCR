0a45a93c42a87c343dc22f89a33d614c89083f28
Kevin Baribeau <kbaribeau@Kevin-Baribeaus-MacBook-Pro.local>
2010-05-30 03:16:37 -0400

    add a bunch of useless-feeling test cases

--- haskell/ocr.hs
+++ haskell/ocr.hs
@@ -2,15 +2,31 @@ module Ocr where
 
 	import Test.HUnit
 
+	recognize :: String -> Int
+	recognize x | x == zero  = 0
+							| x == one   = 1
+							| x == two   = 2
+							| x == three = 3
+							| x == four  = 4
+							| x == five  = 5
+							| x == six   = 6
+							| x == seven = 7
+							| x == eight = 8
+							| x == nine  = 9
 
-	--main = do putStrLn "Hello World!"
+	test0 = TestCase (assertEqual "recognize zero" (recognize zero) 0)
+	test1 = TestCase (assertEqual "recognize one" (recognize one) 1)
+	test2 = TestCase (assertEqual "recognize two" (recognize two) 2)
+	test3 = TestCase (assertEqual "recognize three" (recognize three) 3)
+	test4 = TestCase (assertEqual "recognize four" (recognize four) 4)
+	test5 = TestCase (assertEqual "recognize five" (recognize five) 5)
+	test6 = TestCase (assertEqual "recognize six" (recognize six) 6)
+	test7 = TestCase (assertEqual "recognize seven" (recognize seven) 7)
+	test8 = TestCase (assertEqual "recognize eight" (recognize eight) 8)
+	test9 = TestCase (assertEqual "recognize nine" (recognize nine) 9)
+	tests = TestList [test0, test1, test2, test3, test4, test5, test6, test7, test8, test9]
 
-
-	main = putStrLn zero
-
-	--test1 = TestCase (assertEqual "noop test" True True)
-	--main = runTestTT tests
-	--tests = TestList [test1]
+	main = runTestTT tests
 
 	zero = unlines [
 		" _ ", 
